c060ccd36c34ef7334fb5fce9e0206049606f246
Mr. J.A.Bianchi <MisterBianchi@gmail.com>
2019-01-22 00:05:00 -0500

    second commit

---
+++ src/IntArrayWorker.java
@@ -0,0 +1,138 @@
+public class IntArrayWorker
+{
+  /** two dimensional matrix */
+  private int[][] matrix = null;
+  
+  /** set the matrix to the passed one
+    * @param theMatrix the one to use
+    */
+  public void setMatrix(int[][] theMatrix)
+  {
+    matrix = theMatrix;
+  }
+  
+  /**
+   * Method to return the total 
+   * @return the total of the values in the array
+   */
+  public int getTotal()
+  {
+    int total = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        total = total + matrix[row][col];
+      }
+    }
+    return total;
+  }
+  
+  /**
+   * Method to return the total using a nested for-each loop
+   * @return the total of the values in the array
+   */
+  public int getTotalNested()
+  {
+    int total = 0;
+    for (int[] rowArray : matrix)
+    {
+      for (int item : rowArray)
+      {
+        total = total + item;
+      }
+    }
+    return total;
+  }
+  
+  /**
+   * FULLY IMPLEMENTED IN ACTIVTY #4 (1)
+   * Method to return the count of the number of
+   * times the passed integer appears in the matrix
+   * @param target the integer to count
+   * @return the count
+   */
+  public int getCount(int target)
+  {
+    return -1;
+  }
+  
+  /**
+   * FULLY IMPLEMENTED IN ACTIVITY #4 (2)
+   * Method to return the largest value in
+   * the array
+   * @return the largest value found in the array
+   */
+  public int getLargest()
+  {
+    
+    return -1;
+  }
+  
+  /**
+   * FULLY IMPLEMENTED IN ACTIVITY #4 (3)
+   * Method to return the total of the values in the 
+   * specified column
+   * @param col the column to total
+   * @return the total
+   */
+  public int getColTotal(int col)
+  {
+    return -1;
+  }
+  
+  /**
+   * Method to fill with an increasing count
+   */
+  public void fillCount()
+  {
+    int numCols = matrix[0].length;
+    int count = 1;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < numCols; col++)
+      {
+        matrix[row][col] = count;
+        count++;
+      }
+    }
+  }
+  
+  /**
+   * print the values in the array in rows and columns
+   */
+  public void print()
+  {
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        System.out.print( matrix[row][col] + " " );
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+  
+  
+  /** 
+   * fill the array with a pattern
+   */
+  public void fillPattern1()
+  {
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; 
+           col++)
+      {
+        if (row < col)
+          matrix[row][col] = 1;
+        else if (row == col)
+          matrix[row][col] = 2;
+        else
+          matrix[row][col] = 3;
+      }
+    }
+  }
+ 
+}
---
+++ src/PictureTester.java
@@ -0,0 +1,110 @@
+/**
+ * This class contains class (static) methods
+ * that will help you test the Picture class 
+ * methods.  Uncomment the methods and the code
+ * in the main to test.
+ * 
+ * @author Barbara Ericson 
+ */
+public class PictureTester
+{
+  /** Method to test zeroBlue */
+  public static void testZeroBlue()
+  {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.zeroBlue();
+    beach.explore();
+  }
+  
+  /** Method to test keepOnlyBlue [IMPLEMENTED A5 (3)]  */
+  
+  /** Method to test keepOnlyGreen [EC] */
+    
+  /** Method to test keepOnlyRed [EC] */
+  
+  /** Method to test negate [IMPLEMENTED A5 (4)]*/
+
+  /** Method to test grayscale [IMPLEMENTED A5 (5)]*/
+  
+  /** Method to test fixUnderwater [IMPLEMENTED A5 (6) EC]*/
+  
+  /** Method to test mirrorVertical */
+  public static void testMirrorVertical()
+  {
+    Picture caterpillar = new Picture("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVertical();
+    caterpillar.explore();
+  }
+  
+  /** Method to test mirrorVerticalRightToLeft [IMPLEMENTED A6 (1) ] */
+  
+  /** Method to test mirrorHorizontal [IMPLEMENTED A6 (2) ]*/
+  
+  /** Method to test mirrorHorizontalBotToTop [IMPLEMENTED A6 (3) ]*/
+  
+  /** Method to test mirrorDiagonal [IMPLEMENTED A6 (4) EC]*/
+  
+  /** Method to test mirrorTemple */
+  public static void testMirrorTemple()
+  {
+    Picture temple = new Picture("temple.jpg");
+    temple.explore();
+    temple.mirrorTemple();
+    temple.explore();
+  }
+  
+  /** Method to test mirrorArms [IMPLEMENTED A7 (2) ]*/
+  
+  /** Method to test mirrorGull  [IMPLEMENTED A7 (3) ]*/
+  
+  /** Method to test the collage method */
+  public static void testCollage()
+  {
+    Picture canvas = new Picture("640x480.jpg");
+    canvas.createCollage();
+    canvas.explore();
+  }
+  
+  /** Method to test copy  [IMPLEMENTED A8 (1) ]*/       
+  
+  /** Method to test edgeDetection */
+  public static void testEdgeDetection()
+  {
+    Picture swan = new Picture("swan.jpg");
+    swan.edgeDetection(10);
+    swan.explore();
+  }
+  
+  /** Method to test edgeDetection2  [IMPLEMENTED A9 (2) ]*/
+  
+
+  /** Main method for testing.  Every class can have a main
+    * method in Java */
+  public static void main(String[] args)
+  {
+    // uncomment a call here to run a test and comment
+    // out the ones you don't want to run
+    
+    testZeroBlue();
+    //testKeepOnlyBlue();
+    //testKeepOnlyRed();
+    //testKeepOnlyGreen();
+    //testNegate();
+    //testGrayscale();
+    //testFixUnderwater();
+    //testMirrorVertical();
+    //testMirrorVerticalRightToLeft();
+    //testMirrorHorizontal();
+    //testMirrorHorizontalBotToTop();
+    //testMirrorDiagonal();
+    //testMirrorTemple();
+    //testMirrorArms();
+    //testMirrorGull();
+    //testCollage();
+    //testCopy();
+    //testEdgeDetection();
+    //testEdgeDetection2();
+  }
+}
